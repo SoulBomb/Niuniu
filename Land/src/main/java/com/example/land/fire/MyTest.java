a98631527f1dd1152886f37ea9745ac7b273362b
jhl <soulbomb@163.com>
2022-09-01 21:12:52 +0800

    一点小demo

--- Land/src/main/java/com/example/land/fire/MyTest.java
+++ Land/src/main/java/com/example/land/fire/MyTest.java
@@ -4,22 +4,50 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.util.calendar.BaseCalendar;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyTest {
 
-
+    private static final long DAY_SECONDS = 86400 * 1000;
 
     @Test
     public void ymlTest() {
+        Integer a = 2;
+        Date date = new Date();
+        Timestamp startTime = new Timestamp(date.getTime() - (a * DAY_SECONDS));
+        Timestamp endTime = new Timestamp(date.getTime());
+        System.out.println(startTime);
+        System.out.println(endTime);
+    }
 
+    private long cc(BaseCalendar.Date date) {
+        return date.getDayOfMonth();
     }
 
 
     @Test
     public void ossTest() {
+        ArrayList<String> objects = new ArrayList<>();
+        List<String> asd = objects.stream().filter(a -> a.equals("asd")).collect(Collectors.toList());
+        System.out.println(asd.size());
+    }
 
+    @Test
+    public void ssTest() throws Exception{
+        AtomicReference<String> a = new AtomicReference<>();
+        a.set("asd");
+        System.out.println(a.get());
+        AtomicReference<String> b = new AtomicReference<>("ok");
+        System.out.println(b.get());
     }
 }
 
--- Land/src/main/java/com/example/land/fire/control.java
+++ Land/src/main/java/com/example/land/fire/control.java
@@ -1,28 +1,16 @@
 package com.example.land.fire;
 
-import com.alibaba.nacos.api.annotation.NacosInjected;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
-import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingService;
-import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.example.land.file.FileDemo;
 import com.example.land.oss.OSSDemo;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -77,53 +65,8 @@ public class control {
     }
 
     @GetMapping("qq")
-    public String ll() throws Exception {
-        OkHttpClient okHttpClient = new OkHttpClient();
-//        JSONObject body = new JSONObject();
-//        body.put("appId", "1551474023015591936");
-//        body.put("appSecret", "62de4a66e4b0a6e6309b0e033xa5u4rjeo");
-//        body.put("token", "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzEsInVzZXJfa2V5IjoiYWFjYjJlZTMtZWFiOS00NDliLTk4MmYtZTMwOGRiNGFhZDllIiwidXNlcm5hbWUiOiJsdnNoYW9qdW4ifQ.1KG_6VoX5aeV_iA9Z0LLduQCbfARKRF2EjNyP-MQTverKE8jRPW_3901kxSm6gtQ3RdzMZ_ZqUr79aObzs9K-Q");
-//        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), body.toJSONString());
-//        Request.Builder builder = new Request.Builder();
-//        builder.url("http://211.138.64.10:8089/prod-api/system/api/sys/users/auth/login").post(requestBody);
-//        Map<String, String> headers = new HashMap<>(4);
-//        headers.put("Authorization", "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzEsInVzZXJfa2V5IjoiYWFjYjJlZTMtZWFiOS00NDliLTk4MmYtZTMwOGRiNGFhZDllIiwidXNlcm5hbWUiOiJsdnNoYW9qdW4ifQ.1KG_6VoX5aeV_iA9Z0LLduQCbfARKRF2EjNyP-MQTverKE8jRPW_3901kxSm6gtQ3RdzMZ_ZqUr79aObzs9K-Q");
-//        headers.put("requestId", "qweqw1231231");
-//        if (headers != null) {
-//            builder.headers(Headers.of(headers));
-//        }
-        Request.Builder builder = new Request.Builder();
-        builder.url("http://36.133.154.67:8000/gateway/oss-file/ossNonce/NjNlMDRkM2MzZDE1NG" +
-                "M5YWE5MGQwOTY2MjU1OGU4OWU3NzNhYmM2MDI2YjM0OGEzYWEyNWE3ZjM0NmUyYzUwOTZkZmQwMWE" +
-                "5OTUwOGZhNzkzNGVhOGRjNWFhOGM4YmFl").get();
-        Request request = builder.build();
-        Call eventCall = okHttpClient.newCall(request);
-        Response execute = eventCall.execute();
-        System.out.println(execute);
-        InputStream ii = execute.body().byteStream();
-
-        FileOutputStream ss = new FileOutputStream("C:\\Users\\wanji\\Downloads\\tt.jpg");
-        byte[] bytes = StreamUtils.copyToByteArray(ii);
-        ss.write(bytes);
-        return "string";
-    }
-
-
-    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
-    private boolean useLocalCache;
-
-
-    @GetMapping("get")
-    public boolean get() {
-        return useLocalCache;
-    }
-
-    @NacosInjected
-    private NamingService namingService;
-
-    @GetMapping("gets")
-    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
-        return namingService.getAllInstances(serviceName);
+    public String ll() {
+        return "ok";
     }
 
     @Autowired
@@ -133,7 +76,6 @@ public class control {
     private SpringContextUtil springContextUtil;
 
     @GetMapping("aa")
-
     public String aa() {
         ApplicationContext applicationContext = springContextUtil.getApplicationContext();
         Ad ad = (Ad) applicationContext.getBean("ad");
